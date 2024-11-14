772276c3d254887e81e4782dcca8a20f552ceb9e
the-human-guy <170432371+the-human-guy@users.noreply.github.com>
2024-11-14 23:45:30 +0400

    opt-in html preview

--- noticer/js/pages/aesFileEncrypt/components/fileEditor.js
+++ noticer/js/pages/aesFileEncrypt/components/fileEditor.js
@@ -1,5 +1,4 @@
 const { useEffect, useState } = React
-
 /*
 todo: add more editor options:
 - https://github.com/JiHong88/SunEditor
@@ -8,9 +7,14 @@ todo: add more editor options:
 - https://github.com/bevacqua/woofmark
 */
 
+const PREVIEW_MODE = {
+ HTML: 'html',
+}
+
 export const FileEditor = ({ onSave, file: originalFile }) => {
   const [fileContent, setFileContent] = useState();
-
+  const [previewMode, setPreviewMode] = useState(false);
+ 
   const resetToOriginalFile = () => {
     const reader = new FileReader();
     reader.onload = (event) => {
@@ -40,17 +44,23 @@ ${fileContent}
       <button type="button" onClick={saveFile}>Save changes</button>
       <button type="button" onClick={wrapWithPre}>{'Wrap with <pre>'}</button>
       <button type="reset" onClick={resetToOriginalFile}>Reset</button>
+      <select onChange={e => setPreviewMode(e.target.value)}>
+        <option value={false}>Preview disabled</option>
+        <option value={PREVIEW_MODE.HTML}>HTML</option>
+      </select>
 
       <div class="row">
         <div class="col">
           <textarea onChange={e => setFileContent(e.target.value)} cols="30" rows="33" value={fileContent} style={{ width: '100%' }}></textarea>
         </div>
-        <div class="col editor-preview-container">
-          <iframe
-            sandbox=""
-            srcdoc={fileContent}
-          ></iframe>
-        </div>
+        {previewMode === PREVIEW_MODE.HTML && (
+          <div class="col editor-preview-container">
+            <iframe
+              sandbox=""
+              srcdoc={fileContent}
+            ></iframe>
+          </div>
+        )}
       </div>
     </div>
   )
